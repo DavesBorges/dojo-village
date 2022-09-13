696460c9ba7b7bf3a56ab7721b455541c4a96f71
DavesBorges <66228145+DavesBorges@users.noreply.github.com>
2022-09-13 20:14:40 +0100

    Wire up database

---
+++ src/data/setup_database.ts
@@ -0,0 +1,26 @@
+import { CamelCasePlugin, Kysely, SqliteDialect } from 'kysely';
+import { DB } from './schema-definition';
+import * as dotenv from 'dotenv';
+import Database = require('better-sqlite3');
+dotenv.config();
+
+export let database: Kysely<DB>;
+
+/**
+ * Sets the singleton databaseReference to the db argument or fallback to default
+ * databaseReference if the function is called with no arguments
+ *
+ * @param db the db reference to set as the global database
+ */
+export const setupDatabase = (db?: Kysely<DB>) => {
+  console.log({ DB_PATH: process.env.DB_PATH });
+  if (!db) {
+    db = new Kysely<DB>({
+      dialect: new SqliteDialect({
+        database: new Database(process.env.DB_PATH),
+      }),
+      plugins: [new CamelCasePlugin()],
+    });
+  }
+  database = db;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupDatabase } from './data/setup_database';
 
 async function bootstrap() {
+  setupDatabase();
   const app = await NestFactory.create(AppModule);
   await app.listen(80);
 }
