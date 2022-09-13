282520af683e02d48ef1fc5771fe695ebc0a3b27
DavesBorges <66228145+DavesBorges@users.noreply.github.com>
2022-09-13 20:15:43 +0100

    Clean app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return "Olá Mundo";
-  }
 }
