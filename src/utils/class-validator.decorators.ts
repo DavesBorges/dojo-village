6b7da0c3ce1744bddd24c00347d8762c844e7f4b
DavesBorges <66228145+DavesBorges@users.noreply.github.com>
2023-01-28 02:07:02 +0000

    Add boolean deorators

--- src/utils/class-validator.decorators.ts
+++ src/utils/class-validator.decorators.ts
@@ -1,19 +1,142 @@
 import { applyDecorators } from '@nestjs/common';
-import { Transform, Type } from 'class-transformer';
-import { ValidateIf, ValidateNested, ValidationOptions } from 'class-validator';
+import {
+  Transform,
+  Type,
+  plainToInstance,
+  instanceToPlain,
+} from 'class-transformer';
+
+import {
+  IsArray,
+  IsDefined,
+  ValidateIf,
+  ValidateNested,
+  ValidationOptions,
+} from 'class-validator';
 
 export function IsNullable(validationOptions?: ValidationOptions) {
   return ValidateIf((_object, value) => value !== null, validationOptions);
 }
-export function IsNestedObject(type: any) {
+import {} from 'class-transformer';
+
+export const DB_GROUP = 'db_group';
+export const transformValueOfBoolean = (args: any) => {
+  if (args.value === null) {
+    return null;
+  }
+  const value = args.value;
+  if (typeof value === 'number') {
+    return !!value;
+  }
+
+  if (typeof value === 'string') {
+    if (value === 'base64:type16:AA==') {
+      return false;
+    }
+    if (value === 'base64:type16:AQ==') {
+      return true;
+    }
+
+    if (value === 'false') {
+      return false;
+    }
+
+    if (value === 'true') {
+      return true;
+    }
+  }
+
+  if (Buffer.isBuffer(value)) {
+    return Boolean(Number(value.readUintLE(0, value.length).toString()));
+  }
+
+  return value as boolean;
+};
+
+export const TransformValueOfBoolean = (strict = true) => {
+  const extra = [];
+  if (strict) {
+    extra.push(
+      Transform((args) => Number(args.value), {
+        toPlainOnly: true,
+        groups: [DB_GROUP],
+      }),
+    );
+  }
   return applyDecorators(
     Transform((args) => {
-      if (typeof args.value === 'string') {
-        args.value = JSON.parse(args.value);
+      const value = args.value;
+      if (typeof value === 'number') {
+        return !!value;
       }
-      return args.value && args.value.id === null ? null : args.value;
+
+      if (typeof value === 'string') {
+        if (value === 'base64:type16:AA==') {
+          return false;
+        }
+        if (value === 'base64:type16:AQ==') {
+          return true;
+        }
+
+        if (value === 'false') {
+          return false;
+        }
+
+        if (value === 'true') {
+          return true;
+        }
+      }
+
+      if (Buffer.isBuffer(value)) {
+        return Boolean(Number(value.readUintLE(0, value.length).toString()));
+      }
+
+      return value as boolean;
     }),
-    ValidateNested(),
+    ...extra,
+  );
+};
+
+export function IsNestedObject(
+  type: any,
+  options: { each: boolean } = { each: false },
+  strict = true,
+) {
+  const extra = [];
+  if (options.each) {
+    extra.push(IsArray());
+  }
+  if (strict) {
+    extra.push(
+      Transform(
+        (args) =>
+          args.value
+            ? args.value.length
+              ? instanceToPlain(args.value, { groups: [DB_GROUP] })
+              : `${args.value.id.toString()}`
+            : args.value,
+        {
+          toPlainOnly: true,
+          groups: [DB_GROUP],
+        },
+      ),
+    );
+  }
+  return applyDecorators(
+    Transform(
+      (args) => {
+        if (typeof args.value === 'string') {
+          args.value = JSON.parse(args.value);
+        }
+        return args.value && args.value.id === null
+          ? null
+          : plainToInstance(type, args.value);
+      },
+      { toClassOnly: true },
+    ),
+    ValidateNested(options),
     Type(() => type),
+    IsDefined(),
+    ...extra,
   );
 }
